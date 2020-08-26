b4883e35010dc8e3825f78171e8afd9ab3ba4ac7
Dr.Lunyak <wampwar11@gmail.com>
2020-08-26 19:30:06 +0300

    media head

--- js/main.js
+++ js/main.js
@@ -1,60 +1,193 @@
-
 // добавляем и убираем активные состояние у меню навигации
-$(".nav ul li a").click(function(e) {
+$(".nav ul li a").click(function (e) {
   e.preventDefault();
-  $(".nav ul li a").removeClass('nav__links-active');
-  $(this).addClass('nav__links-active');
-})
+  $(".nav ul li a").removeClass("nav__links-active");
+  $(this).addClass("nav__links-active");
+});
 
-jQuery(document).ready(function($){
-	var contentSections = $('.cd-section'),
-		navigationItems = $('#cd-vertical-nav a');
+jQuery(document).ready(function ($) {
+  var contentSections = $(".cd-section"),
+    navigationItems = $("#cd-vertical-nav a");
 
-	updateNavigation();
-	$(window).on('scroll', function(){
-		updateNavigation();
+  updateNavigation();
+  $(window).on("scroll", function () {
+    updateNavigation();
+  });
+
+  //smooth scroll to the section
+  navigationItems.on("click", function (event) {
+    event.preventDefault();
+    smoothScroll($(this.hash));
+  });
+  //smooth scroll to second section
+  $(".cd-scroll-down").on("click", function (event) {
+    event.preventDefault();
+    smoothScroll($(this.hash));
+  });
+
+  //open-close navigation on touch devices
+  $(".touch .cd-nav-trigger").on("click", function () {
+    $(".touch #cd-vertical-nav").toggleClass("open");
+  });
+  //close navigation on touch devices when selectin an elemnt from the list
+  $(".touch #cd-vertical-nav a").on("click", function () {
+    $(".touch #cd-vertical-nav").removeClass("open");
 	});
 
-	//smooth scroll to the section
-	navigationItems.on('click', function(event){
-        event.preventDefault();
-        smoothScroll($(this.hash));
-    });
-    //smooth scroll to second section
-    $('.cd-scroll-down').on('click', function(event){
-        event.preventDefault();
-        smoothScroll($(this.hash));
-    });
+	const a1 = 'a1'
+	const a2 = 'a2'
+	
+	const aaa = a1 + ' 111 ' + a2 + ' 222'
+	const bbb = `${a1} 111 ${a2} 222`
 
-    //open-close navigation on touch devices
-    $('.touch .cd-nav-trigger').on('click', function(){
-    $('.touch #cd-vertical-nav').toggleClass('open');
+  function updateNavigation() {
+    contentSections.each(function () {
+      $this = $(this);
+      var activeSection =
+        $('#cd-vertical-nav a[href="#' + $this.attr("id") + '"]').data(
+          "number"
+        ) - 1;
+      if (
+        $this.offset().top - $(window).height() / 2 < $(window).scrollTop() &&
+        $this.offset().top + $this.height() - $(window).height() / 2 >
+          $(window).scrollTop()
+      ) {
+        navigationItems.eq(activeSection).addClass("is-selected");
+      } else {
+        navigationItems.eq(activeSection).removeClass("is-selected");
+      }
     });
-    //close navigation on touch devices when selectin an elemnt from the list
-    $('.touch #cd-vertical-nav a').on('click', function(){
-    	$('.touch #cd-vertical-nav').removeClass('open');
-    });
+  }
 
-	function updateNavigation() {
-		contentSections.each(function(){
-			$this = $(this);
-			var activeSection = $('#cd-vertical-nav a[href="#'+$this.attr('id')+'"]').data('number') - 1;
-			if ( ( $this.offset().top - $(window).height()/2 < $(window).scrollTop() ) && ( $this.offset().top + $this.height() - $(window).height()/2 > $(window).scrollTop() ) ) {
-				navigationItems.eq(activeSection).addClass('is-selected');
-			}else {
-				navigationItems.eq(activeSection).removeClass('is-selected');
-			}
-		});
-	}
+  function smoothScroll(target) {
+    $("body,html").animate({ scrollTop: target.offset().top }, 600);
+  }
+});
 
-	function smoothScroll(target) {
-        $('body,html').animate(
-        	{'scrollTop':target.offset().top},
-        	600
-        );
-	}
+/*fixed Header */
+$(function () {
+  let nav = $(".header");
+  let windowWidth = $(window).innerWidth();
+  let content = $(".main");
+  let contentH = content.innerHeight();
+  let scrollPos = $(window).scrollTop();
+  // let NavToggle = $("#NavToggle");
+
+  // checkScroll(scrollPos, contentH);
+
+  $(window).on("scroll resize", function () {
+    scrollPos = $(this).scrollTop();
+
+    if (scrollPos > contentH - 80) {
+      nav.addClass("fixed");
+    } else {
+      nav.removeClass("fixed");
+    }
+  });
 });
 
 
 
 
+// перемещаю номер телефона и корзинку на высоту равную блоку навигации
+let widthNav = $(".nav").height();
+console.log(widthNav);
+$(".header__phone").css({
+  top: widthNav,
+});
+// $(".header__buy-icon").css({
+//   top: widthNav,
+// });
+
+
+// BURGER
+
+
+
+
+$(".header-burder-open").on("click", function () {
+	$(".nav").fadeIn();
+	$(this).fadeOut();
+	$(".header-burder-close").fadeIn();
+	$(".header__buy-icon").css({
+		top: widthNav,
+	});
+	$(".header__phone").fadeIn();
+	$('.logo__wrap').css({
+		"margin-right": "0",
+	});
+	$('.logo__wrap h2').css({
+		"color": "#3F3F3F",
+	});
+	$('.logo__wrap img').css({
+		"display": "none",
+	});
+	$('.burger-logo').css({
+		"display": "block",
+	});
+	$('.header').css({
+		"background": "#282828",
+	});
+});
+
+
+$(".header-burder-close").on("click", function () {
+	$(".nav").fadeOut();
+	$(this).fadeOut();
+	$(".header-burder-open").fadeIn();
+	$(".header__phone").fadeOut();
+	$(".header__buy-icon").css({
+		top: 32,
+	});
+	$('.logo__wrap').css({
+		"margin-right": "30px",
+	});
+	$('.logo__wrap h2').css({
+		"color": "#fff",
+	});
+	$('.logo__wrap img').css({
+		"display": "block",
+	});
+	$('.burger-logo').css({
+		"display": "none",
+	});
+		$('.header').css({
+		"background": "#28282800",
+	});
+});
+
+// let burger = true;
+
+// function navBig() {
+//       if (burger) {
+//         $(".burger-off").toggleClass("burder-transform");
+//         $(".burger-off").css({
+//           "opacity": "0",
+//           "visibility": "hidden"
+//         });
+//         $(".burger-on").toggleClass("burder-transform");
+//         $(".burger-on").css({
+//           "opacity": "1",
+//           "visibility": "visible"
+//         });
+//         $(".nav").slideDown();
+//         $(".link-list-drop-company, .link-list-drop-services, .link-list-drop-technology").fadeIn();
+
+//         burger = false;
+//     } else {
+
+//       $(".burger-off").toggleClass("burder-transform");
+//       $(".burger-off").css({
+//         "opacity": "1",
+//         "visibility": "visible"
+//       });
+//       $(".burger-on").toggleClass("burder-transform");
+//       $(".burger-on").css({
+//         "opacity": "0",
+//         "visibility": "hidden"
+//       });
+//       $(".nav").slideUp(200);
+//       $(".link-list-drop-company, .link-list-drop-services, .link-list-drop-technology").fadeOut(200);
+//       burger = true;
+//   };
+// };
